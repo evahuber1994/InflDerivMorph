d8062104b31ed83696291a79df51a0ae77bbb930
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-10-05 14:57:13 +0200

    update

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from data_reader import read_deriv, FeatureExtractor, RelationsDataLoader, SimpleDataLoader
 from rank_evaluation import Ranker
 from model import BasicFeedForward, RelationFeedForward
-from utils import make_vocabulary_matrix, cosine_distance_loss
+from utils import make_vocabulary_matrix, cosine_distance_loss, create_dict, save_new
 
 
 
