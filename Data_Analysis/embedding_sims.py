2d6b6d58136b1e1874540ec81d97565d6c76ffcd
evahuber1994 <eva.huber@student.uni-tuebingen.de>
2020-10-14 11:50:47 +0200

    update

--- Data_Analysis/embedding_sims.py
+++ Data_Analysis/embedding_sims.py
@@ -1,15 +1,44 @@
 from data_reader import read_deriv, FeatureExtractor
 from sklearn.metrics.pairwise import cosine_similarity
-file = '/home/evahu/Documents/Master/Master_Dissertation/InflDerivMorph/data/rus_data_conll/DERIVATION_TH50/Derivbase_finalfilter_thresh80.csv'
-emb_path = '/embeddings/rus_65/model.fifu'
-out_path = '/home/evahu/Documents/Master/Master_Dissertation/InflDerivMorph/data/rus_data_conll/DERIVATION_TH50/embedding_sims'
-rels, w1s, w2s = read_deriv(file)
+import pandas as pd
+import numpy as np
 
-feature_extractor = FeatureExtractor(emb_path, 100)
-with open(out_path, 'w') as wf:
-    for w1, w2, r in zip(w1s, w2s, rels):
-        w1_emb = feature_extractor.get_embedding(w1)
-        w2_emb = feature_extractor.get_embedding(w2)
-        sim = cosine_similarity(w1_emb.reshape(1,-1), w2_emb.reshape(1,-1))
-        wf.write("{}\t{}\t{}\t{}\n".format(r, w1, w2, str(round(sim[0][0], 2))))
 
+
+def get_sim_per_word(path, out_path, emb_path):
+
+    feature_extractor = FeatureExtractor(emb_path, 100)
+    rels, w1s, w2s = read_deriv(path)
+    with open(out_path, 'w') as wf:
+        wf.write("{}\t{}\t{}\t{}\n".format("relation", "w1", "w2", "cos_sim"))
+        for w1, w2, r in zip(w1s, w2s, rels):
+            w1_emb = feature_extractor.get_embedding(w1)
+            w2_emb = feature_extractor.get_embedding(w2)
+            sim = cosine_similarity(w1_emb.reshape(1,-1), w2_emb.reshape(1,-1))
+            wf.write("{}\t{}\t{}\t{}\n".format(r, w1, w2, str(round(sim[0][0], 2))))
+
+def get_sim_per_relation(in_path):
+    out_path = in_path.replace(".csv", "_avg.csv")
+    rel_dict = dict()
+    with open(in_path, 'r') as rf:
+        next(rf)
+        for l in rf:
+            l = l.strip()
+            line = l.split("\t")
+            if line[0] not in rel_dict:
+                rel_dict[line[0]] = []
+            rel_dict[line[0]].append(float(line[3]))
+    with open(out_path, 'w') as wf:
+        wf.write("{}\t{}\n".format("relation", "average_sim"))
+        for k,v in rel_dict.items():
+            wf.write("{}\t{}\n".format(k, np.mean(v)))
+
+def main():
+    file = '/home/evahu/Documents/Master/Master_Dissertation/InflDerivMorph/data/FINAL/DE/normal_o1o/combined_train.csv'
+    emb_path = '../embeddings/de_45/model.fifu'
+    out_path = '/home/evahu/Documents/Master/Master_Dissertation/InflDerivMorph/data/FINAL/DE/emb_sims_word.csv'
+    #get_sim_per_word(file, out_path, emb_path)
+    get_sim_per_relation(out_path)
+
+if __name__ == "__main__":
+    main()
--- Data_Analysis/offset_vectors.py
+++ Data_Analysis/offset_vectors.py
@@ -3,14 +3,18 @@ from sklearn.metrics.pairwise import cosine_similarity
 from utils import create_dict
 import numpy as np
 import random
-def offset_vectors(file_path, embedding_path, emb_dim, out_path):
+def offset_vectors(file_path, embedding_path, emb_dim, out_path, threshold):
     dict_rel = create_dict(file_path)
     extractor = FeatureExtractor(embedding_path, emb_dim)
     dict_matrices = dict()
 
     for k,v in dict_rel.items():
+        length = len(v)
+        if threshold > 0:
+            if len(v) > threshold:
+                length = threshold
         print(k)
-        matr_cur = np.zeros((1000, emb_dim))
+        matr_cur = np.zeros((length, emb_dim))
         random.shuffle(v)
         for i, w in enumerate(v[:1000]):
             matr_cur[i] =np.subtract(extractor.get_embedding(w[0]), extractor.get_embedding(w[1]))
@@ -20,26 +24,46 @@ def offset_vectors(file_path, embedding_path, emb_dim, out_path):
     with open(out_path, 'w') as wf:
         wf.write("{}\t{}\n".format("relation", "average_offset_similarity"))
         for k,v in dict_matrices.items():
+
             print(k)
             print(v.shape)
             sims = []
             for i in range(v.shape[0]):
                 for j in range(v.shape[0]):
                     if i != j:
-                        sims.append(cosine_similarity(v[i].reshape(-1,1), v[j].reshape(-1,1)))
+                        cs = cosine_similarity(v[i].reshape(1,-1), v[j].reshape(1,-1))
+                        sims.append(cs[0][0])
+
             wf.write("{}\t{}\n".format(k, str(np.mean(sims))))
+def embedding_sims(path, e_path, e_dim, out_path, average=True):
+    dict_rel = create_dict(path)
+    avg_sims = {k: [] for k in dict_rel.keys()}
+    extractor = FeatureExtractor(e_path, e_dim)
+    with open(out_path, 'w') as wf:
+        wf.write("{}\t{}\t{}\t{}\n".format("relation", "w1", "w2", "sim"))
+        for k,v in dict_rel.items():
+            for w in v:
+                sim = cosine_similarity(extractor.get_embedding(w[0]).reshape(1,-1), extractor.get_embedding(w[1]).reshape(1,-1))
+                sim = round(sim[0][0],3)
+                avg_sims[k].append(sim)
+                wf.write("{}\t{}\t{}\t{}\n".format(k, w[0], w[1], str(sim)))
+    if average:
+        out_path2 = out_path.replace(".csv", "_average.csv")
+        with open(out_path2, 'w') as wf:
+            wf.write("{}\t{}\n".format("relation", "average_sim"))
+            for k,v in avg_sims.items():
+                avg = np.mean(v)
+                wf.write("{}\t{}\n".format(k, str(round(avg,3))))
+
+
 
 
 def main():
     path = 'FINAL/DE/normal_o1o/combined_train.csv'
     e_path = 'de_conll_data/model.fifu'
-    out_path = 'embedding_measures/de_normalo1o_train.csv'
-    avg_sims = offset_vectors(path, e_path, 100, out_path)
-    print("avg sims computed")
-    with open(out_path, 'w') as wf:
-        wf.write("{}\t{}\n".format("relation", "average_offset_similarity"))
-        for k,v in avg_sims.items():
-            wf.write("{}\t{}\n".format(k, str(v)))
+    out_path = 'embedding_measures/de_normal_o1o_embedding_sims.csv'
+    threshold = 1000
+    embedding_sims(path, e_path, 100, out_path)
 
 if __name__ == "__main__":
     main()
